9a3a305a529e0541bd3ca3059795ebb0483a2936
Tri Pham <tri.pham121@icloud.com>
2025-02-17 16:41:39 -0500

    image sorter now have basic functioning by moving all image files to hard-coded images folder

--- image_sorter.c
+++ image_sorter.c
@@ -4,22 +4,86 @@
 #include <dirent.h>  // For directory traversal functions like opendir, readdir, etc.
 #include <sys/stat.h> // For file status functions like stat.
 #include <sys/stat.h> // For getting image information
+#include <errno.h>
 
 #pragma region Function Prototypes
 char* GetDesktopPath(void);
 DIR* OpenDirectory(char* path);
 int IsImageFile(const char* filename);
-void PrintImageInDirectory(DIR* dir);
+void MoveFilesToFolder(DIR* sourceDir, char* fpath);
+char* GetSortedFolderPath(char* fname);
+void MoveFile(const char* sourcePath, const char* destinationPath);
+void MakeFolder(const char* fpath);
 #pragma endregion
+
+//static variable
+static char* homePath;
+static char* folderPath;
 
 int main(void) {
     printf("Sorting program starting...\n");
 
-    char* homeDirectory = GetDesktopPath();
-    DIR* sortDirectory = OpenDirectory(homeDirectory);
-    PrintImageInDirectory(sortDirectory);
-    free(homeDirectory);
+    homePath = GetDesktopPath();
+    folderPath = GetSortedFolderPath("images");
+    MakeFolder(folderPath);
+
+    DIR* sortDirectory = OpenDirectory(homePath);
+    MoveFilesToFolder(sortDirectory, folderPath);
+    free(homePath);
+    free(folderPath);
     return 0;
+}
+
+void MakeFolder(const char* fpath) {
+    if (mkdir(fpath, 0777) == -1) {
+        if (errno != EEXIST) {
+            perror("Error creating folder");
+            exit(1);
+        } else {
+            printf("Folder already exists: %s\n", fpath);
+        }
+    } else {
+        printf("Created folder: %s\n", fpath);
+    }
+}
+
+void MoveFilesToFolder(DIR* sourceDir, char* fpath) {
+    struct dirent* entry;
+    char srcFilePath[1024];
+    char dstFilePath[1024];
+
+    while ((entry = readdir(sourceDir)) != NULL) {
+        if (
+            entry->d_type == DT_REG 
+            && IsImageFile(entry->d_name)
+        ) { 
+            printf("Found image: %s\n", entry->d_name);
+            snprintf(srcFilePath, sizeof(srcFilePath), "%s/%s", homePath, entry->d_name);
+
+            snprintf(dstFilePath, sizeof(dstFilePath), "%s/%s", fpath, entry->d_name);
+            MoveFile(srcFilePath, dstFilePath);
+        }
+    }
+}
+
+void MoveFile(const char* sourcePath, const char* destinationPath) {
+    if (rename(sourcePath, destinationPath) == 0) {
+        printf("Moved: %s -> %s\n", sourcePath, destinationPath);
+    } else {
+        perror("Error moving file");
+    }
+}
+
+char* GetSortedFolderPath(char* fname) {
+    size_t pathLen = strlen(homePath) + strlen(fname) + 2;
+    char* folderPath = (char*)malloc(pathLen);
+    if (folderPath == NULL) {
+        perror("Error allocating memory for sorted folder path");
+        exit(1);
+    }
+
+    snprintf(folderPath, pathLen, "%s/%s", homePath, fname);
+    return folderPath;
 }
 
 char* GetDesktopPath(void){
@@ -65,15 +129,15 @@ int IsImageFile(const char* filename) {
     return 0;
 }
 
-void PrintImageInDirectory(DIR* dir) {
-    struct dirent* entry;
+// void PrintImageInDirectory(DIR* dir) {
+//     struct dirent* entry;
 
-    while ((entry = readdir(dir)) != NULL) {
-        if (
-            entry->d_type == DT_REG 
-            && IsImageFile(entry->d_name)
-        ) { 
-            printf("Found image: %s\n", entry->d_name);
-        }
-    }
-}
+//     while ((entry = readdir(dir)) != NULL) {
+//         if (
+//             entry->d_type == DT_REG 
+//             && IsImageFile(entry->d_name)
+//         ) { 
+//             printf("Found image: %s\n", entry->d_name);
+//         }
+//     }
+// }
